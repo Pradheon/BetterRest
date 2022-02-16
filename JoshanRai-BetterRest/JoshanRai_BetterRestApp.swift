00382ec86690e9cb00dac935aab9e483bac7a653
Joshan Rai <pradheon@gmail.com>
2022-02-15 23:36:05 -0800

    Initial Commit

---
+++ JoshanRai-BetterRest/JoshanRai_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_BetterRestApp.swift
+//  JoshanRai-BetterRest
+//
+//  Created by Joshan Rai on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
